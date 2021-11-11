cbe67b676ca3f6dbb828a3067aa487417993ebb1
Chanwoo <tree698@gmail.com>
2021-11-11 14:27:55 +0900

    Study of arrays, formatted string and Random library

---
+++ Python/Rock_Paper_Scissors/Topics/Elif statement/Computer hours/main.py
@@ -0,0 +1,7 @@
+a = int(input())
+if a < 2:
+    print("That's rare nowadays!")
+elif 2 <= a < 4:
+    print("This seems reasonable")
+else:
+    print("Don't forget to take breaks!")
---
+++ Python/Rock_Paper_Scissors/Topics/Elif statement/Particles/main.py
@@ -0,0 +1,13 @@
+first = input()
+second = input()
+
+if first == '1/2' and second == '-1/3':
+    print('Strange Quark')
+elif first == '1/2' and second == '2/3':
+    print('Charm Quark')
+elif first == '1/2' and second == '-1':
+    print('Electron Lepton')
+elif first == '1/2' and second == '0':
+    print('Neutrino Lepton')
+elif first == '1' and second == '0':
+    print('Photon Boson')
