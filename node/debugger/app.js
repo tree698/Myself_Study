dda3437aa9fad96ba6d1f07b22276216e7899246
Chanwoo <tree698@gmail.com>
2021-12-28 13:18:11 +0900

    Study of debugger

---
+++ node/debugger/app.js
@@ -0,0 +1,24 @@
+function sayHello() {
+  console.log('hello');
+  console.log('sdf');
+  console.log('sdfs');
+}
+
+function calculate(x, y) {
+  console.log('calculating...');
+  const result = x + y;
+  console.log('result: ', result);
+  sayHello();
+  return result;
+}
+
+calculate(1, 7);
+
+const stop = 4;
+console.log('...looping...');
+for (let i = 0; i < 10; i++) {
+  console.log('count', i);
+  if (i === stop) {
+    break;
+  }
+}
