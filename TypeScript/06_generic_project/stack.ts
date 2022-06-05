3a261b94e7f844260e47200ac909fc0cb10d2504
Chanwoo <tree698@gmail.com>
2022-06-05 15:28:54 +0900

    implement stack with generic

--- TypeScript/06_generic_project/stack.ts
+++ TypeScript/06_generic_project/stack.ts
@@ -1,62 +1,63 @@
 {
-	interface Stack<T> {
-		readonly size: number;
-		push(value: T): void;
-		pop(): T;
-	}
+  interface Stack<T> {
+    readonly size: number;
+    push(value: T): void;
+    pop(): T;
+  }
 
-	type StackNode<T> = {
-		readonly value: T;
-		readonly next?: StackNode<T>;
-	};
+  type StackNode<T> = {
+    readonly value: T;
+    readonly next?: StackNode<T>;
+  };
 
-	class StackImpl<T> implements Stack<T> {
-		private _size: number = 0;
-		private head?: StackNode<T>;
+  class StackImpl<T> implements Stack<T> {
+    private _size: number = 0;
+    private head?: StackNode<T>;
 
-		constructor(private capacity: number) {}
-		get size() {
-			return this._size;
-		}
+    constructor(private capacity: number) {}
+    get size() {
+      return this._size;
+    }
 
-		push(value: T): void {
-			if (this.size === this.capacity) {
-				throw new Error('Stack is full');
-			}
-			// 타입 추론 StackNode<T> 생략
-			const node = { value, next: this.head };
-			this.head = node;
-			this._size++;
-		}
+    push(value: T): void {
+      if (this.size === this.capacity) {
+        throw new Error('Stack is full');
+      }
+      // 타입 추론 StackNode<T> 생략
+      const node = { value, next: this.head };
+      this.head = node;
+      this._size++;
+    }
 
-		// this.head === undefined  =>  null일 경우 통과됨
-		// null == undefined  <==>  null !== undefined
-		pop(): T {
-			if (this.head == null) {
-				throw new Error('Stack is empty');
-			}
-			const node = this.head;
-			this.head = node.next;
-			this._size--;
-			return node.value;
-		}
-	}
+    // this.head === undefined  =>  null일 경우 통과됨
+    // null == undefined  <==>  null !== undefined
+    pop(): T {
+      if (this.head == null) {
+        throw new Error('Stack is empty');
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
 
-	const stack = new StackImpl<string>(10);
-	stack.push('ellie 1');
-	stack.push('steve 2');
-	stack.push('Bob 3');
+  // interface를 generic으로 정의하고 해당 객체에서 타입을 지정함으로써 재사용성을 높임
+  const stack = new StackImpl<string>(10);
+  stack.push('ellie 1');
+  stack.push('steve 2');
+  stack.push('Bob 3');
 
-	while (stack.size !== 0) {
-		console.log(stack.pop());
-	}
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
 
-	const stack2 = new StackImpl<number>(10);
-	stack2.push(123);
-	stack2.push(456);
-	stack2.push(789);
+  const stack2 = new StackImpl<number>(10);
+  stack2.push(123);
+  stack2.push(456);
+  stack2.push(789);
 
-	while (stack2.size != 0) {
-		console.log(stack2.pop());
-	}
+  while (stack2.size != 0) {
+    console.log(stack2.pop());
+  }
 }
