ac9ceaba8ce0806f06b62902ff02717cc976fc5f
Chanwoo <tree698@gmail.com>
2022-06-14 17:40:42 +0900

    practice for typeScript

--- TypeScript/02_type_projects/calculator.ts
+++ TypeScript/02_type_projects/calculator.ts
@@ -4,20 +4,20 @@
 
 type Operator = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
 function calculate(command: Operator, num1: number, num2: number): number {
-	switch (command) {
-		case 'add':
-			return num1 + num2;
-		case 'substract':
-			return num1 - num2;
-		case 'multiply':
-			return num1 * num2;
-		case 'divide':
-			return num1 / num2;
-		case 'remainder':
-			return num1 % num2;
-		default:
-			throw new Error('Wrong Number');
-	}
+  switch (command) {
+    case 'add':
+      return num1 + num2;
+    case 'substract':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    case 'remainder':
+      return num1 % num2;
+    default:
+      throw new Error('Wrong Number');
+  }
 }
 
 console.log(calculate('add', 1, 3)); // 4
@@ -25,3 +25,25 @@ console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
+
+type operator = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculateAgain(
+  operator: operator,
+  num1: number,
+  num2: number
+): number {
+  switch (operator) {
+    case 'add':
+      return num1 + num2;
+    case 'substract':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    case 'remainder':
+      return num1 % num2;
+    default:
+      throw new Error('wrong number');
+  }
+}
--- TypeScript/02_type_projects/game.ts
+++ TypeScript/02_type_projects/game.ts
@@ -3,33 +3,33 @@
  */
 
 const position = {
-	x: 0,
-	y: 0,
+  x: 0,
+  y: 0,
 };
 
 type Direction = 'up' | 'down' | 'left' | 'right';
 
 function move(direction: Direction) {
-	switch (direction) {
-		case 'up':
-			// return (position.y += 1);
-			position.y += 1;
-			break;
-		case 'down':
-			// return (position.y -= 1);
-			position.y -= 1;
-			break;
-		case 'left':
-			// return (position.x -= 1);
-			position.x -= 1;
-			break;
-		case 'right':
-			// return (position.x += 1);
-			position.x += 1;
-			break;
-		default:
-			throw new Error(`Wrong direction: ${direction}`);
-	}
+  switch (direction) {
+    case 'up':
+      // return (position.y += 1);
+      position.y += 1;
+      break;
+    case 'down':
+      // return (position.y -= 1);
+      position.y -= 1;
+      break;
+    case 'left':
+      // return (position.x -= 1);
+      position.x -= 1;
+      break;
+    case 'right':
+      // return (position.x += 1);
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`Wrong direction: ${direction}`);
+  }
 }
 
 console.log(position); // { x: 0, y: 0}
@@ -43,3 +43,33 @@ move('right');
 console.log(position); // { x: 0, y: 0}
 move('r');
 console.log(position); // { x: 0, y: 0}
+
+type direction = 'up' | 'down' | 'left' | 'right';
+type coordinate = {
+  x: number;
+  y: number;
+};
+
+let pos: coordinate = {
+  x: 0,
+  y: 0,
+};
+
+function moveAgain(command: direction): coordinate {
+  switch (command) {
+    case 'up':
+      pos.y += 1;
+      break;
+    case 'down':
+      pos.y -= 1;
+      break;
+    case 'left':
+      pos.x += 1;
+      break;
+    case 'right':
+      pos.x -= 1;
+      break;
+    default:
+      throw new Error('wrong position');
+  }
+}
